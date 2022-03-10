eae0320c9121ff625512bbe84a6c195fb0eccbb5
LUH <ung7497@ajou.ac.kr>
2022-03-10 17:34:46 +0900

    Initial Commit

---
+++ HelloSwift/HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftApp.swift
+//  HelloSwift
+//
+//  Created by unghee Lee on 2022/03/10.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
