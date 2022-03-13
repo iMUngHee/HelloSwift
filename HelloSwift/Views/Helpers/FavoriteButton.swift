55765b14bf4c1af80f8b670cc944197174542746
LUH <ung7497@ajou.ac.kr>
2022-03-13 21:54:21 +0900

    챕터3!

---
+++ HelloSwift/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  HelloSwift
+//
+//  Created by unghee Lee on 2022/03/13.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toogle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
